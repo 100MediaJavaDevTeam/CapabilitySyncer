77faa654594690ca10eee9ce332fa2394db8452d
SizableShrimp <sizableshrimp@sizableshrimp.me>
2022-01-12 12:19:37 -0600

    Backport of features from 1.18
    Update forge version
    Update parchment

--- src/main/java/me/sizableshrimp/capabilitysyncer/core/CapabilityAttacher.java
+++ src/main/java/me/sizableshrimp/capabilitysyncer/core/CapabilityAttacher.java
@@ -3,6 +3,7 @@ package me.sizableshrimp.capabilitysyncer.core;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
 import net.minecraft.util.Direction;
@@ -37,7 +38,8 @@ public abstract class CapabilityAttacher {
     };
 
     static {
-        MinecraftForge.EVENT_BUS.addGenericListener(Entity.class, CapabilityAttacher::onAttachCapability);
+        MinecraftForge.EVENT_BUS.addGenericListener(Entity.class, CapabilityAttacher::onAttachEntityCapability);
+        MinecraftForge.EVENT_BUS.addGenericListener(ItemStack.class, CapabilityAttacher::onAttachItemStackCapability);
         MinecraftForge.EVENT_BUS.addListener(CapabilityAttacher::onEntityJoinWorld);
         MinecraftForge.EVENT_BUS.addListener(CapabilityAttacher::onPlayerStartTracking);
         MinecraftForge.EVENT_BUS.addListener(CapabilityAttacher::onPlayerClone);
@@ -48,27 +50,37 @@ public abstract class CapabilityAttacher {
         CapabilityManager.INSTANCE.register(capClass, (Capability.IStorage<T>) EMPTY_STORAGE, () -> null);
     }
 
-    private static final List<BiConsumer<AttachCapabilitiesEvent<Entity>, Entity>> capAttachers = new ArrayList<>();
-    private static final List<Function<Entity, LazyOptional<? extends ISyncableCapability>>> capRetrievers = new ArrayList<>();
-    private static final List<BiConsumer<PlayerEntity, PlayerEntity>> capCloners = new ArrayList<>();
+    private static final List<BiConsumer<AttachCapabilitiesEvent<Entity>, Entity>> entityCapAttachers = new ArrayList<>();
+    private static final List<Function<Entity, LazyOptional<? extends ISyncableCapability>>> entityCapRetrievers = new ArrayList<>();
+
+    private static final List<BiConsumer<AttachCapabilitiesEvent<ItemStack>, ItemStack>> itemStackCapAttachers = new ArrayList<>();
+    private static final List<Function<ItemStack, LazyOptional<? extends ItemStackCapability>>> itemStackCapRetrievers = new ArrayList<>();
+
+    private static final List<BiConsumer<PlayerEntity, PlayerEntity>> playerCapCloners = new ArrayList<>();
 
     protected static <C extends ISyncableCapability> void registerPlayerAttacher(BiConsumer<AttachCapabilitiesEvent<Entity>, PlayerEntity> attacher,
             Function<PlayerEntity, LazyOptional<C>> capRetriever, boolean copyOnDeath) {
-        registerAttacher(PlayerEntity.class, attacher, capRetriever);
+        registerEntityAttacher(PlayerEntity.class, attacher, capRetriever);
         if (copyOnDeath) {
-            capCloners.add((oldPlayer, newPlayer) -> capRetriever.apply(oldPlayer).ifPresent(oldCap -> capRetriever.apply(newPlayer)
+            playerCapCloners.add((oldPlayer, newPlayer) -> capRetriever.apply(oldPlayer).ifPresent(oldCap -> capRetriever.apply(newPlayer)
                     .ifPresent(newCap -> newCap.deserializeNBT(oldCap.serializeNBT(false), false))));
         }
     }
 
     @SuppressWarnings("unchecked")
-    protected static <E extends Entity, C extends ISyncableCapability> void registerAttacher(Class<E> entityClass, BiConsumer<AttachCapabilitiesEvent<Entity>, E> attacher,
+    protected static <E extends Entity, C extends ISyncableCapability> void registerEntityAttacher(Class<E> entityClass, BiConsumer<AttachCapabilitiesEvent<Entity>, E> attacher,
             Function<E, LazyOptional<C>> capRetriever) {
-        capAttachers.add((event, entity) -> {
+        entityCapAttachers.add((event, entity) -> {
             if (entityClass.isInstance(entity))
                 attacher.accept(event, (E) entity);
         });
-        capRetrievers.add(entity -> entityClass.isInstance(entity) ? capRetriever.apply((E) entity) : LazyOptional.empty());
+        entityCapRetrievers.add(entity -> entityClass.isInstance(entity) ? capRetriever.apply((E) entity) : LazyOptional.empty());
+    }
+
+    protected static <C extends ItemStackCapability> void registerItemStackAttacher(BiConsumer<AttachCapabilitiesEvent<ItemStack>, ItemStack> attacher,
+            Function<ItemStack, LazyOptional<C>> capRetriever) {
+        itemStackCapAttachers.add(attacher);
+        itemStackCapRetrievers.add(capRetriever::apply);
     }
 
     protected static <I extends INBTSerializable<T>, T extends INBT> void genericAttachCapability(AttachCapabilitiesEvent<?> event, I impl, Capability<I> capability, ResourceLocation location) {
@@ -117,23 +129,28 @@ public abstract class CapabilityAttacher {
         };
     }
 
-    private static void onAttachCapability(AttachCapabilitiesEvent<Entity> event) {
-        // Attaches the capabilities
-        capAttachers.forEach(attacher -> attacher.accept(event, event.getObject()));
+    private static void onAttachEntityCapability(AttachCapabilitiesEvent<Entity> event) {
+        // Attaches the entity capabilities
+        entityCapAttachers.forEach(attacher -> attacher.accept(event, event.getObject()));
+    }
+
+    private static void onAttachItemStackCapability(AttachCapabilitiesEvent<ItemStack> event) {
+        // Attaches the item stack capabilities
+        itemStackCapAttachers.forEach(attacher -> attacher.accept(event, event.getObject()));
     }
 
     private static void onEntityJoinWorld(EntityJoinWorldEvent event) {
         if (event.getEntity() instanceof ServerPlayerEntity) {
             // Syncs a player's capabilities to themselves on world join (either joining server or switching worlds)
             ServerPlayerEntity serverPlayer = (ServerPlayerEntity) event.getEntity();
-            capRetrievers.forEach(capRetriever -> capRetriever.apply(serverPlayer).ifPresent(cap -> cap.sendUpdatePacketToPlayer(serverPlayer)));
+            entityCapRetrievers.forEach(capRetriever -> capRetriever.apply(serverPlayer).ifPresent(cap -> cap.sendUpdatePacketToPlayer(serverPlayer)));
         }
     }
 
     private static void onPlayerStartTracking(PlayerEvent.StartTracking event) {
         // Syncs an entity's capabilities to a player when they start tracking it
         ServerPlayerEntity currentPlayer = (ServerPlayerEntity) event.getPlayer();
-        capRetrievers.forEach(capRetriever -> capRetriever.apply(event.getTarget()).ifPresent(cap -> cap.sendUpdatePacketToPlayer(currentPlayer)));
+        entityCapRetrievers.forEach(capRetriever -> capRetriever.apply(event.getTarget()).ifPresent(cap -> cap.sendUpdatePacketToPlayer(currentPlayer)));
     }
 
     private static void onPlayerClone(PlayerEvent.Clone event) {
@@ -143,6 +160,6 @@ public abstract class CapabilityAttacher {
         // So we can copy capabilities
         oldPlayer.revive();
 
-        capCloners.forEach(capCloner -> capCloner.accept(oldPlayer, newPlayer));
+        playerCapCloners.forEach(capCloner -> capCloner.accept(oldPlayer, newPlayer));
     }
 }
---
+++ src/main/java/me/sizableshrimp/capabilitysyncer/core/ItemStackCapability.java
@@ -0,0 +1,12 @@
+package me.sizableshrimp.capabilitysyncer.core;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+
+public abstract class ItemStackCapability implements INBTSavable<CompoundNBT> {
+    protected final ItemStack itemStack;
+
+    protected ItemStackCapability(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+}
